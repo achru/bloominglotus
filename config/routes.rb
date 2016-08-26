930221ccdb99b1c3f645fb4f98435c82ddfd4c31
Prelang Builder <builder@prelang.com>
2016-08-26 12:31:38 +0000

    Generated scaffolding for model 'School' via Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :schools
+
   resources :event_types
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
